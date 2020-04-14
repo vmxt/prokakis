c3a4a52da3e9a27742cc25e3f386e15370a6c925
Daryl <darylyrad.cabz@gmail.com>
2020-04-14 08:39:21 +0800

    added new banner

--- resources/views/oppor/explore.blade.php
+++ resources/views/oppor/explore.blade.php
@@ -247,6 +247,11 @@
            box-shadow: 0 8px 16px 0 rgba(0, 0, 0, 1);
          }
 
+         .premium_banner{
+            float: right;
+            width: 65%;
+            padding-bottom: 15px;
+         }
         /*.card-explore{
             height: 25em;
         }*/
@@ -340,8 +345,11 @@ foreach ($build as $item) {
                     <div class="thumbnail" style="margin-bottom: 5px;">
 
                         <?php if ($item->view_type != 2) {?>
+                        <div>
                         <img class="premium_banner" alt="Premium Banner" 
-                             src="{{ asset('public/banner/banner-premium.png') }}">
+                             src="{{ asset('public/banner/banner-new4.png') }}">
+                         </div>
+                         <br>
                         <img class="card-img-top img-circle" alt="profile image" style="border: saddlebrown"
                              src="{{ asset('public/images/') }}/<?php echo $avat ?>">
 
